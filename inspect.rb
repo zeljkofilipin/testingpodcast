43decae1353b31e4324d757a5fbc4528170019a4
Zeljko Filipin <zeljko.filipin@gmail.com>
2011-10-04 14:30:03 +0200

    get post numbers

--- inspect.rb
+++ inspect.rb
@@ -1,6 +1,19 @@
+def log_in(browser, site)
+  browser.goto "#{site}wp-login.php"
+  browser.text_field(:id => "user_login").set "" # enter user name
+  browser.text_field(:id => "user_pass").set "" # enter password
+  browser.button(:id => "wp-submit").click
+end
+def get_post_numbers(browser, site)
+  browser.goto site
+  browser.divs(:class => "post").collect do |post|
+    post.a(:text => "Edit").href.split("=")[1].split("&")[0]
+  end
+end
 require "watir-webdriver"
 
 site = "http://testingpodcast.com/"
 browser = Watir::Browser.new :ff
 
-browser.goto site
+log_in(browser, site)
+puts get_post_numbers(browser, site).inspect
