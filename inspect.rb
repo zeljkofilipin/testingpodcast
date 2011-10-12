de4d4ec1ce254ea93fc9ff48d4e13be2d9c9f01d
Zeljko Filipin <zeljko.filipin@gmail.com>
2011-10-12 23:52:08 +0200

    using log_in method from login.rb

--- inspect.rb
+++ inspect.rb
@@ -1,9 +1,3 @@
-def log_in(browser, site, password)
-  browser.goto "#{site}wp-login.php"
-  browser.text_field(:id => "user_login").set "zeljko.filipin"
-  browser.text_field(:id => "user_pass").set password
-  browser.button(:id => "wp-submit").click
-end
 def categories(browser)
   browser.ul(:id => "categorychecklist").labels.collect do |label|
     label.text if label.input.checked?
@@ -45,6 +39,7 @@ if __FILE__ == $0
   password = ARGV[0]
   browser = Watir::Browser.new :ff
 
+  require "login"
   log_in(browser, site, password)
   puts "index@url@title@permalink@text@categories@tags@mp3_location@mp3_title@mp3_type@mp3_file_size@mp3_duration@mp3_itunes_author"
   post_numbers(browser, site).each_with_index do |post_number, index|
